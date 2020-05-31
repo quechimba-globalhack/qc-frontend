7fdf1046f1b6debe40e69e9b2efc6e03d2fede24
jlriiot <ing.jmontana@gmail.com>
2020-05-31 08:34:29 -0500

    Createing getExperience query

---
+++ src/components/pages/experience/api/queries/experience.ts
@@ -0,0 +1,63 @@
+import { gql } from "apollo-boost";
+
+export interface Auction {
+  actn_id: number;
+  exp_id: number;
+  start_date: "2020-10-28T17:01:20";
+  duration: number;
+  start_value: "5000.0000 LKS";
+  highest_bid: number;
+  bkn: string;
+  req_cancel: number;
+  created_at: Date;
+}
+
+export interface Experience {
+  exp_id: number;
+  bkn_id: string;
+  content: string;
+  start_date: string;
+  places: number;
+  base_val: number;
+  maxactntdate: Date;
+  engagement: number;
+  pub_price: number;
+  sealed: number;
+  auction: Auction;
+}
+
+export interface ExperienceData {
+  experiences: Array<Experience>;
+}
+
+export interface GetExperienceVariables {
+  expid: number;
+}
+
+export const GET_EXPERIENCE_QUERY = gql`
+  query getExperience($expid: Int!) {
+    expereiences(expid: $expid) {
+      exp_id
+      bkn_id
+      content
+      start_date
+      places
+      base_val
+      maxactntdate
+      engagement
+      pub_price
+      sealed
+      auction {
+        actn_id
+        exp_id
+        start_date
+        duration
+        start_value
+        highest_bid
+        bkn
+        req_cancel
+        created_at
+      }
+    }
+  }
+`;
