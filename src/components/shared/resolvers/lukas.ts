b12328aea6cdd92a6d47a8182e6f8af00fb296b3
jlriiot <ing.jmontana@gmail.com>
2020-05-31 10:13:21 -0500

    Creating balance query

---
+++ src/components/shared/resolvers/lukas.ts
@@ -0,0 +1,21 @@
+import { gql } from "apollo-boost";
+
+export interface Lukas {
+  balance: string | null;
+}
+
+export interface LukasData {
+  getBalance: Lukas;
+}
+
+export interface GetBalanceVariables {
+  user: string;
+}
+
+export const GET_BALANCE_QUERY = gql`
+  query getBalance($user: String!) {
+    getBalace(user: $user) {
+      balance
+    }
+  }
+`;
